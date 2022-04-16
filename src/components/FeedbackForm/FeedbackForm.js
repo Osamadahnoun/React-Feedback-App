d53ba280c0a78d2207e7a4cc5a2bf35468fe034f
Osama Dahnoun <omdahnou@ncsu.edu>
2022-04-16 03:02:19 -0400

    can add review and rating

--- src/components/FeedbackForm/FeedbackForm.js
+++ src/components/FeedbackForm/FeedbackForm.js
@@ -3,7 +3,7 @@ import Card from "../shared/Card";
 import Button from "../shared/Button";
 import RatingSelect from "../RatingSelect/RatingSelect";
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -26,12 +26,24 @@ const FeedbackForm = () => {
     setText(event.target.value);
   };
   //   console.log(text);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      handleAdd(newFeedback);
+
+      setText("");
+    }
+  };
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect />
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             type="text"
--- src/components/FeedbackList/FeedbackList.js
+++ src/components/FeedbackList/FeedbackList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "../FeedbackItem/FeedbackItem";
 import PropTypes from "prop-types";
 
@@ -6,13 +7,33 @@ const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <div key={item.id}>
+  //         <FeedbackItem item={item} handleDelete={handleDelete} />
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <div key={item.id}>
-          <FeedbackItem item={item} handleDelete={handleDelete} />
-        </div>
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
